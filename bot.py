6264bd8bea888929ac89075a62be9beb0b40a9fe
RostislavGordeev <144150642+Ruen189@users.noreply.github.com>
2025-04-25 02:52:40 +0500

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,50 +1,98 @@
 import telebot
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
 from dotenv import load_dotenv
+from ai_operator import get_answer
 import os
+import sys
+import time
+import logging
+from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
+import datetime
+
+logging.basicConfig(filename='log.txt', level=logging.INFO)
+
+logging.basicConfig(
+    filename='log.txt',
+    level=logging.INFO,
+    format='%(asctime)s - %(message)s',
+    encoding='utf-8'
+)
 
 load_dotenv()
 # 1. Инициализация бота
 bot = telebot.TeleBot(os.getenv("TOKEN"))
 
-# 2. Шаблонные вопросы и ответы
-faq_data = {
-    "Как заселиться в общежитие?": "Для заселения вам нужно обратиться в студгородок с паспортом и студенческим билетом",
-    "Какие документы нужны для заселения?": "Паспорт, студенческий билет, договор на обучение и медицинская справка.",
-    "Сколько стоит проживание?": "Стоимость проживания зависит от типа общежития, обычно от 1000 до 3000 руб./мес.",
-    "Можно ли выбрать комнату?": "Выбор комнаты ограничен, уточните в деканате или студгородке.",
-}
-
-questions = list(faq_data.keys())
-answers = list(faq_data.values())
-
-# 3. NLP-модель (TF-IDF)
-vectorizer = TfidfVectorizer().fit(questions)
-question_vectors = vectorizer.transform(questions)
-
-
-# 4. Обработка сообщений
+# 2. Обработка сообщений
 @bot.message_handler(commands=['start'])
 def handle_start(message):
     bot.send_message(message.chat.id, "Здравствуйте, какой у вас вопрос?")
 
+@bot.callback_query_handler(func=lambda call: str(call.from_user.id) == os.getenv("ADMIN_ID"))
+def handle_admin_callbacks(call):
+    if call.data == "log":
+        with open("log.txt", "rb") as f:
+            bot.send_document(call.message.chat.id, f)
+    elif call.data == "clear_log":
+        try:
+            admin = call.from_user
+            with open("log.txt", "w", encoding="utf-8") as f:
+                f.write(
+                    f"[{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}] logs was cleared by: @{admin.username} (ID: {admin.id})\n")
+
+            bot.send_message(call.message.chat.id, "🧹 Логи успешно очищены.")
+        except Exception as e:
+            bot.send_message(call.message.chat.id, f"❌ Ошибка при очистке логов: {e}")
+    elif call.data == "off":
+        confirm_markup = InlineKeyboardMarkup()
+        confirm_markup.add(
+            InlineKeyboardButton("✅ Да", callback_data="confirm_off"),
+            InlineKeyboardButton("❌ Нет", callback_data="cancel_off")
+        )
+        bot.send_message(call.message.chat.id, "Вы уверены, что хотите отключить бота?",
+                         reply_markup=confirm_markup)
+    elif call.data == "confirm_off":
+        username = call.from_user.username or "unknown"
+        admin_id = call.from_user.id
+
+        try:
+            with open("log.txt", "a", encoding="utf-8") as f:
+                f.write(
+                    f"{datetime.datetime.now()} - Bot was turned off by @{username} (ID: {admin_id})\n")
+
+            bot.send_message(call.message.chat.id, "🔄 Выключение бота...")
+            time.sleep(1)
+            python = sys.executable
+            os.execl(python, python, *sys.argv)
+
+        except Exception as e:
+            bot.send_message(call.message.chat.id, f"Error off: {e}")
+
+    elif call.data == "cancel_off":
+        bot.send_message(call.message.chat.id, "❌ Отключение отменено.")
+
+@bot.message_handler(commands=['admin'])
+def admin_panel(message):
+    if str(message.from_user.id) != os.getenv("ADMIN_ID"):
+        bot.send_message(message.chat.id, "⛔ У вас нет доступа.")
+        return
+
+    markup = InlineKeyboardMarkup()
+    markup.add(InlineKeyboardButton("📥 Скачать лог", callback_data="log"))
+    markup.add(InlineKeyboardButton("🧹 Очистить лог", callback_data="clear_log"))
+    markup.add(InlineKeyboardButton("❌ Выключить бота", callback_data="off"))
+    bot.send_message(message.chat.id, "🛠 Админ-панель", reply_markup=markup)
+
 @bot.message_handler(func=lambda message: True)
 def handle_message(message):
     user_input = message.text
-    user_vector = vectorizer.transform([user_input])
-    similarities = cosine_similarity(user_vector, question_vectors)
-
-    best_match_index = similarities.argmax()
-    confidence = similarities[0, best_match_index]
-
-    if confidence > 0.5:
-        response = answers[best_match_index]
+    response, score, isFound = get_answer(user_input)
+    if isFound:
+        bot.send_message(message.chat.id, response)
     else:
-        response = "Извините, я не понял ваш вопрос. Вы можете его уточнить у " + os.getenv("OPERATOR")
+        bot.send_message(message.chat.id, response + os.getenv("OPERATOR"))
+        user = message.from_user
+        log_entry = (f"Time: {datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')} | User: @{user.username} | ID: {user.id} | is_bot: "
+                     f"{user.is_bot} | Message: {user_input} | score: {score}")
+        logging.info(log_entry)
 
-    bot.send_message(message.chat.id, response)
-
-
-# 5. Запуск
+# 3. Запуск
 bot.polling()
